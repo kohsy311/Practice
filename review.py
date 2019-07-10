673c9deddce628f6349faef6e29448700b04566b
Clara <kohsy311@gmail.com>
2019-07-10 12:31:57 +0900

    first commit

---
+++ review.py
@@ -0,0 +1,22 @@
+# import webbrowser
+# webbrowser.open("https://www.daum.net/")
+# webbrowser.open_new("http://www.msbsound.com/about/")
+# webbrowser.open_new_tab("https://www.google.com/")
+
+#import webbrowser
+#webbrowser.open('https://search.naver.com/search.naver?query=')
+#love=["치즈","레드벨벳","오마이걸","러블리즈"]
+#for idol in love:
+    #print(type(idol))
+    #webbrowser.open('https://search.naver.com/search.naver?query='+idol)
+
+import webbrowser
+webbrowser.open("https://search.naver.com/search.naver?query=")
+
+keyword=["치즈", "잔나비", "선우정아"]
+
+for love in keyword:
+    print(love)
+    webbrowser.open("https://search.naver.com/search.naver?query="+love)
+
+
